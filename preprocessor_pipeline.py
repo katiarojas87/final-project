06ab7b6bc57f71844a3b764c7d5d76c0b4871aa8
Lancelot Tabiaat <176578554+Ltabiaat@users.noreply.github.com>
2026-03-05 14:01:46 +0900

    added the structure of  and almost finished the preprocess pipeline

--- preprocessor_pipeline.py
+++ preprocessor_pipeline.py
@@ -1,5 +1,4 @@
 #core
-!pip install scikit-learn --break-system-packages -q
 import pandas as pd
 import pathlib
 
@@ -21,37 +20,44 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import TargetEncoder
 
-# Model selection
-from sklearn.model_selection import train_test_split, cross_val_score
 
-#load dataset(double check name of the main dataframe)
-data_path = pathlib.Path(".")
-data = pd.read_csv(data_path / "raw_data/listings_with_scores.csv")
-data.head()
 
-#define features X and target
-X = data.drop(columns=["price_man_yen"]).copy()
-y = data["price_man_yen"]
+def preprocess_features(X):
+    """
+    This function creates a preprocessor pipeline and returns X_processed.
+    """
+    def create_sklearn_preprocessor() -> ColumnTransformer:
+        num_features = ["price_man_yen","area_sqm","year_built","floor_number","floors_total","walk_minutes"]
 
-# train test split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+        num_transformer = Pipeline ([
+            # ("imputer", SimpleImputer(strategy="mean")), #Missing values, normally distributed
+            # ("standard_scaler", StandardScaler()), #Features on different scales, linear models
+            # ("minmax_scaler", MinMaxScaler()), #When you need values between 0–1
+            ("robust_scaler", RobustScaler()) #Data with lots of outliers
+        ])
 
-#(f"X_train\t{X_train.shape}", f"X_test\t{X_test.shape}", f"y_train\t{y_train.shape}", f"y_test\t{y_test.shape}")
+        cat_features = ["layout","address","nearest_station"]
 
-num_transformer = Pipeline ([
-    ("imputer", SimpleImputer(strategy="mean")), #Missing values, normally distributed
-    ("standard_scaler", StandardScaler()), #Features on different scales, linear models
-    ("minmax_scaler", MinMaxScaler()), #When you need values between 0–1
-    ("robust_scaler", RobustScaler()) #Data with lots of outliers
-])
-cat_transformer = Pipeline ([
-    ("ohe", OneHotEncoder(drop = "if_binary",handle_unknown="ignore")), #Nominal data (no order) e.g. region, room_type
-    ("ordinal_encoder", OrdinalEncoder()), #Ordered categories e.g. small < medium < large
-    ("imputer", SimpleImputer(strategy="most_frequent")) #Missing categorical values
-])
+        cat_transformer = Pipeline ([
+            ("ohe", OneHotEncoder(drop = "if_binary",handle_unknown="ignore")), #Nominal data (no order) e.g. region, room_type
+            ("ordinal_encoder", OrdinalEncoder()), #Ordered categories e.g. small < medium < large
+            ("imputer", SimpleImputer(strategy="most_frequent")) #Missing categorical values
+        ])
 
-# Preprocessing "num_transformer" and "cat_transfomer"
-preprocessor = ColumnTransformer([
-    ('num_transformer', num_transformer, ['column1', 'column2']),
-    ('cat_transformer', cat_transformer, ['column1', 'column2'])
-])
+
+        final_preprocessor = ColumnTransformer([
+            ('num_transformer', num_transformer, num_features),
+            ('cat_transformer', cat_transformer, cat_features)
+        ])
+
+        return final_preprocessor
+
+
+    print("\nPreprocessing features...")
+
+    preprocessor = create_sklearn_preprocessor()
+    X_processed = preprocessor.fit_transform(X)
+
+    print("✅ X_processed, with shape", X_processed.shape)
+
+    return X_processed
